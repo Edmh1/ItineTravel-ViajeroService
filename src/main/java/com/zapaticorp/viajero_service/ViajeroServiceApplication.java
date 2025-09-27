5ea5c682ab6ff539cffde8d4ff29b11c3394fbd8
Edmh <edavidmanotas@gmail.com>
2025-09-27 02:59:19 -0500

    chore: Estructura inicial del proyecto

---
+++ src/main/java/com/zapaticorp/viajero_service/ViajeroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zapaticorp.viajero_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajeroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajeroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zapaticorp/viajero_service/ViajeroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapaticorp.viajero_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajeroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
